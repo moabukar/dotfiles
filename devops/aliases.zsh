a67398acc045835f8f3ddfaae1c4ffd8aba7d94b
moabukar <mo-abukar10@hotmail.com>
2023-07-02 22:10:08 +0100

    add devops aliases: tf, docker & ansible

---
+++ devops/aliases.zsh
@@ -0,0 +1,30 @@
+# Docker
+
+alias d='docker'
+alias dc='docker-compose'
+alias di='docker images'
+alias dps='docker ps'
+alias dpsa='docker ps -a'
+alias drm='docker rm'
+alias drmi='docker rmi'
+alias dlogs='docker logs'
+alias dexec='docker exec -it'
+
+# Terraform
+
+alias tf='terraform'
+alias tfa='terraform apply'
+alias tfd='terraform destroy'
+alias tfs='terraform show'
+alias tfp='terraform plan'
+alias tfi='terraform init'
+alias tfws='terraform workspace'
+
+# Ansible
+
+alias a='ansible'
+alias ap='ansible-playbook'
+alias av='ansible-vault'
+alias ag='ansible-galaxy'
+alias ainv='ansible-inventory'
+alias al='ansible-lint'
