05327beb92004f18ea353fe36a540eed984964a1
moabukar <mo-abukar10@hotmail.com>
2023-07-02 22:07:49 +0100

    add git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -6,19 +6,38 @@ then
   alias git=$hub_path
 fi
 
-# The rest of my fun git aliases
-alias gl='git pull --prune'
-alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias gp='git push origin HEAD'
-
-# Remove `+` and `-` from start of diff lines; just rely upon color.
-alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'
-
-alias gc='git commit'
-alias gca='git commit -a'
-alias gco='git checkout'
-alias gcb='git copy-branch-name'
+# My Git aliases
+alias gs='git status'
+alias ga='git add'
 alias gb='git branch'
-alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
-alias gac='git add -A && git commit -m'
-alias ge='git-edit-new'
+alias gco='git checkout'
+alias gcob='git checkout -b'
+alias gcm='git commit -m'
+alias gcam='git commit -am'
+alias gl='git log'
+alias gp='git pull'
+alias gps='git push'
+alias gd='git diff'
+alias gds='git diff --staged'
+alias gr='git remote'
+alias gra='git remote add'
+alias grr='git remote remove'
+alias gf='git fetch'
+alias gfp='git fetch --prune'
+alias gcl='git clone'
+alias gmv='git mv'
+alias grb='git rebase'
+alias grbc='git rebase --continue'
+alias grbs='git rebase --skip'
+alias grab='git rebase --abort'
+alias gt='git tag'
+alias gta='git tag -a'
+alias gtd='git tag -d'
+alias gtv='git verify-tag'
+alias gch='git cherry-pick'
+alias gcha='git cherry-pick --abort'
+alias gchc='git cherry-pick --continue'
+alias gsh='git stash'
+alias gsha='git stash apply'
+alias gsph='git stash pop'
+alias gsl='git stash list'
