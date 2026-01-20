ecb9cdd01f73e6a4fc0a59b6184346072f40bf99
Mohamed Abukar <mo-abukar10@hotmail.com>
2026-01-20 16:52:47 +0000

    defaults

--- scripts/defaults.sh
+++ scripts/defaults.sh
@@ -19,8 +19,23 @@ defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
 #
 # ====================
 
+# Set Dock size to small
+defaults write com.apple.dock tilesize -int 36
+
 # Disable animation at application launch
 defaults write com.apple.dock launchanim -bool false
+
+# Automatically hide and show the Dock
+defaults write com.apple.dock autohide -bool true
+
+# Show indicators for open applications
+defaults write com.apple.dock show-process-indicators -bool true
+
+# Don't show recent applications in Dock
+defaults write com.apple.dock show-recents -bool false
+
+# Minimize windows using genie effect
+defaults write com.apple.dock mineffect -string "genie"
 
 # ====================
 #
@@ -54,6 +69,44 @@ defaults write com.apple.menuextra.clock DateFormat -string 'EEE d MMM HH:mm'
 
 # Display battery level in the menu bar
 defaults write com.apple.menuextra.battery ShowPercent -string "YES"
+
+# ====================
+#
+# Trackpad
+#
+# ====================
+
+# Enable tap to click
+defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
+# Secondary click: bottom right corner
+defaults write com.apple.AppleMultitouchTrackpad TrackpadCornerSecondaryClick -int 2
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadCornerSecondaryClick -int 2
+defaults -currentHost write NSGlobalDomain com.apple.trackpad.enableSecondaryClick -bool true
+
+# Disable natural scrolling (traditional scroll direction)
+defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
+
+# Trackpad speed (0-3, higher is faster)
+defaults write NSGlobalDomain com.apple.trackpad.scaling -float 2.5
+
+# ====================
+#
+# Mouse
+#
+# ====================
+
+# Disable natural scrolling for mouse too
+defaults write -g com.apple.swipescrolldirection -bool false
+
+# Mouse speed (0-3, higher is faster)
+defaults write NSGlobalDomain com.apple.mouse.scaling -float 2.5
+
+# Enable secondary click (right-click) for mouse
+defaults write com.apple.driver.AppleBluetoothMultitouch.mouse MouseButtonMode -string "TwoButton"
 
 for app in "Dock" \
 	"Finder" \
